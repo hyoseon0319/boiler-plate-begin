5307dac7c0831cd4cf7cc32171b6fa92c83bb7dd
hyoseon0319 <hyosun0319@gmail.com>
2021-04-09 14:16:52 +0900

    비밀 정보 보호

--- models/User.js
+++ models/User.js
@@ -17,5 +17,16 @@ const userSchema = mongoose.Schema({
     role: {
         type: Number,
         default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
     }
 })
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
